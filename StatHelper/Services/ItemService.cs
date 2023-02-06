b34b002b39a0e7b35c8498ed301fe59cf0fdd404
Jan Garretsen <j.garretsen@gmsnl.com>
2023-02-06 12:45:00 +0100

    data will now save locally

--- StatHelper/Services/ItemService.cs
+++ StatHelper/Services/ItemService.cs
@@ -1,7 +1,12 @@
-﻿using StatHelper.ViewModels;
+﻿using StatHelper.Models;
+using StatHelper.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +21,74 @@ namespace StatHelper.Services
         public ItemService()
         {
             Items = new ObservableCollection<ItemViewModel>();
+            Items.CollectionChanged += new NotifyCollectionChangedEventHandler(CollectionChangedMethod);
+
+            LoadItems();
         }
+
+        private void LoadItems()
+        {
+            List<string> lines = File.ReadAllLines("Resources/Data.csv").ToList();
+
+            DataTable dt = new DataTable();
+            
+            if (lines.Count > 0)
+            {
+                foreach (string column in lines[0].Split(','))
+                {
+                    DataColumn col = new DataColumn();
+                    col.ColumnName = column.ToString();
+                    dt.Columns.Add(col);
+                }
+
+                lines.RemoveAt(0);
+
+                foreach (string line in lines)
+                {
+                    DataRow row = dt.NewRow();
+
+                    string[] values = line.Split(',');
+
+                    row.ItemArray = values;
+                    dt.Rows.Add(row);
+                }
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                ItemViewModel item = new ItemViewModel(new Item(Int32.Parse(row["Attack"].ToString()), Int32.Parse(row["CritDmg"].ToString()), Equipment.FromName(row["Equipment"].ToString())));
+                Items.Add(item);
+            }
+
+        }
+
+        private void SaveItems()
+        {
+            List<string> lines = new();
+            lines.Add("Attack,CritDmg,Equipment");
+            foreach (ItemViewModel item in Items)
+            {
+                lines.Add($"{item.Attack},{item.CritDmg},{item.Equipment.Name}");
+            }
+            File.WriteAllLines("Resources/Data.csv", lines);
+        }
+
+
+        private void CollectionChangedMethod(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            foreach (ItemViewModel item in e.NewItems)
+            {
+                item.PropertyChanged += Item_PropertyChanged;
+            }
+            SaveItems();
+        }
+
+        private void Item_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            SaveItems();
+        }
+
+
+
     }
 }
